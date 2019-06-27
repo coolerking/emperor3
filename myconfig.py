e4d544d046fe22bf8f3ae4c624c6d9cfeb728cfd
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2019-06-27 15:51:07 +0900

    旧emperor設定を上書き

--- myconfig.py
+++ myconfig.py
@@ -41,12 +41,19 @@
 # STEERING_CHANNEL = 1            #channel on the 9685 pwm board 0-15
 # STEERING_LEFT_PWM = 460         #pwm value for full left steering
 # STEERING_RIGHT_PWM = 290        #pwm value for full right steering
+STEERING_CHANNEL = 1
+STEERING_LEFT_PWM = 340
+STEERING_RIGHT_PWM = 500
 # 
 # #THROTTLE
 # THROTTLE_CHANNEL = 0            #channel on the 9685 pwm board 0-15
 # THROTTLE_FORWARD_PWM = 500      #pwm value for max forward throttle
 # THROTTLE_STOPPED_PWM = 370      #pwm value for no movement
 # THROTTLE_REVERSE_PWM = 220      #pwm value for max reverse throttle
+THROTTLE_CHANNEL = 0
+THROTTLE_FORWARD_PWM = 450
+THROTTLE_STOPPED_PWM = 390
+THROTTLE_REVERSE_PWM = 330
 # 
 # #DC_STEER_THROTTLE with one motor as steering, one as drive
 # #these GPIO pinouts are only used for the DRIVE_TRAIN_TYPE=DC_STEER_THROTTLE
